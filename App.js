a13026b085401a0ede5efa65b4cd7316d46768aa
SayeedMahdi <sayeedmahdimousavi789@gmail.com>
2026-02-25 19:31:20 +0430

    feat: i work on part of meal item

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="light" />
-      <CategoriesScreen />
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
@@ -14,3 +24,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {},
 });
+
+// npx expo start --localhost
---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,26 @@
+import { StyleSheet, FlatList, View } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
+
+const MealsOverviewScreen = ({ route }) => {
+  const catId = route.params.categoryId;
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
+  return (
+    <View>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
+    </View>
+  );
+};
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({});
